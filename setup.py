0a67e346fccf4ed9e88fd27956c74ce804154308
Kye <kye@apacmediasolutions.com>
2023-05-05 21:51:36 -0400

    setup py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'nebula-loss',
   packages = find_packages(exclude=[]),
-  version = '0.3.0',
+  version = '0.2.0',
   license='MIT',
   description = '1 Loss Function to rule them all!',
   author = 'Agora',
